d59ec26a5ba1aaa527bcafa99458768ca4b04ed4
root <root@vmi70128.contabo.host>
2018-03-28 17:36:36 -0300

    modified:   elasticsearch/elastic-howto-api.sh

--- elasticsearch/elastic-howto-api.sh
+++ elasticsearch/elastic-howto-api.sh
@@ -24,30 +24,29 @@ curl --user $username:$password -XPUT -H "Content-Type: application/json" "$elas
 END
 
 
-#for i in "$(mysql -u root -h 91.205.173.217 --password=mujebik@ -D moodle -e "select mdl_user.username,mdl_user.email,mdl_logstore_standard_log.action,from_unixtime(mdl_logstore_standard_log.timecreated, '%Y-%m-%d') as data_acesso, mdl_logstore_standard_log.courseid, mdl_course.fullname, mdl_logstore_standard_log.other, mdl_user.id, mdl_user.firstname, mdl_user.lastname, mdl_user.city from  mdl_logstore_standard_log, mdl_user, mdl_course where mdl_user.id = mdl_logstore_standard_log.userid and mdl_logstore_standard_log.action = 'viewed' and mdl_logstore_standard_log.courseid = mdl_course.id" | grep -v username | tr \\t ';' | cut -d ';' -f 1,2,4,5,6,9- | sed s/';}'/'|'/g | tr -d '|' | grep -i -v 's:'| grep -i $data_hoje)"
-#do
-#login=$(echo "$i"| cut -d ';' -f 1)
-#email=$(echo "$i"| cut -d ';' -f 2)
-#date=$(echo "$i"| cut -d ';' -f 3)
-#coursename=$(echo "$i"| cut -d ';' -f 5)
-#name=$(echo "$i"| cut -d ';' -f 7)
-#surname=$(echo "$i"| cut -d ';' -f 8)
-#city=$(echo "$i"| cut -d ';' -f 9)
+for i in "$(mysql -u root -h 91.205.173.217 --password=mujebik@ -D moodle -e "select mdl_user.username,mdl_user.email,mdl_logstore_standard_log.action,from_unixtime(mdl_logstore_standard_log.timecreated, '%Y-%m-%d') as data_acesso, mdl_logstore_standard_log.courseid, mdl_course.fullname, mdl_logstore_standard_log.other, mdl_user.id, mdl_user.firstname, mdl_user.lastname, mdl_user.city from  mdl_logstore_standard_log, mdl_user, mdl_course where from_unixtime(mdl_logstore_standard_log.timecreated, '%Y-%m-%d') = "$data_hoje" and  mdl_user.id = mdl_logstore_standard_log.userid and mdl_logstore_standard_log.action = 'viewed' and mdl_logstore_standard_log.courseid = mdl_course.id" | grep -v username | tr \\t ';' | cut -d ';' -f 1,2,4,5,6,9- | sed s/';}'/'|'/g | tr -d '|' | grep -i -v 's:')"
+do
+login=$(echo "$i"| cut -d ';' -f 1)
+email=$(echo "$i"| cut -d ';' -f 2)
+coursename=$(echo "$i"| cut -d ';' -f 5)
+name=$(echo "$i"| cut -d ';' -f 7)
+surname=$(echo "$i"| cut -d ';' -f 8)
+city=$(echo "$i"| cut -d ';' -f 9)
 
 
 
 
-#curl --user $username:$password -XPOST -H 'Content-Type: application/json' "$elastichost:$elasticport/$index/doc?" \
-#		--data @- <<END 
-#{"login": "$login","email": "$email","data": "$date","curso": "$coursename","nome": "$name","sobrenome": "$surname","cidade": "$city"}
-#END
-
-
-#done
-
-
-curl --user $username:$password -XPOST -H 'Content-Type: application/json' "$elastichost:$elasticport/$index/doc" \
+curl --user $username:$password -XPOST -H 'Content-Type: application/json' "$elastichost:$elasticport/$index/doc?" \
 		--data @- <<END 
-{"id": "1", "login": "axl","email": "bguno@merda","data": "$data_agora","curso": "De Merda","nome": "Cú","sobrenome": "de Oliveira","cidade": "Nilopolis"}
+{"login": "$login","email": "$email","data": "$data_agora","curso": "$coursename","nome": "$name","sobrenome": "$surname","cidade": "$city"}
 END
 
+
+done
+
+
+#curl --user $username:$password -XPOST -H 'Content-Type: application/json' "$elastichost:$elasticport/$index/doc" \
+#		--data @- <<END 
+#{"id": "1", "login": "axl","email": "bguno@merda","data": "$data_agora","curso": "De Merda","nome": "Cú","sobrenome": "de Oliveira","cidade": "Nilopolis"}
+#END
+#
