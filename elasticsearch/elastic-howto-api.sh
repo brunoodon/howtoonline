f5da73a7537c6383851393f7f7d7d7124c620e5b
root <root@vmi70128.contabo.host>
2018-03-28 00:01:01 -0300

    modified:   elasticsearch/elastic-howto-api.sh

--- elasticsearch/elastic-howto-api.sh
+++ elasticsearch/elastic-howto-api.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-#if test -f $log_tmp
-#then
-#	rm -f $log_tmp
-#fi
-lista=$(mysql -u root -h 91.205.173.217 --password=mujebik@ -D moodle -e "select mdl_user.username,mdl_user.email,mdl_logstore_standard_log.action,from_unixtime(mdl_logstore_standard_log.timecreated, '%Y-%m-%d') as data_acesso, mdl_logstore_standard_log.courseid, mdl_course.fullname, mdl_logstore_standard_log.other, mdl_user.id, mdl_user.firstname, mdl_user.lastname, mdl_user.city from  mdl_logstore_standard_log, mdl_user, mdl_course where mdl_user.id = mdl_logstore_standard_log.userid and mdl_logstore_standard_log.action = 'viewed' and mdl_logstore_standard_log.courseid = mdl_course.id" | grep -v username | tr \\t ';' | cut -d ';' -f 1,2,4,5,6,9- | sed s/';}'/'|'/g | tr -d '|' | grep -i -v 's:')
+
+data_hoje=$(date +%Y-%m-%d)
+elastichost=elk.howtoonline.com.br
+elasticport=9200
+username=elastic
+password=mujebik@
+index=howto-$data_hoje
+
+
+lista=$(mysql -u root -h 91.205.173.217 --password=mujebik@ -D moodle -e "select mdl_user.username,mdl_user.email,mdl_logstore_standard_log.action,from_unixtime(mdl_logstore_standard_log.timecreated, '%Y-%m-%d') as data_acesso, mdl_logstore_standard_log.courseid, mdl_course.fullname, mdl_logstore_standard_log.other, mdl_user.id, mdl_user.firstname, mdl_user.lastname, mdl_user.city from  mdl_logstore_standard_log, mdl_user, mdl_course where mdl_user.id = mdl_logstore_standard_log.userid and mdl_logstore_standard_log.action = 'viewed' and mdl_logstore_standard_log.courseid = mdl_course.id" | grep -v username | tr \\t ';' | cut -d ';' -f 1,2,4,5,6,9- | sed s/';}'/'|'/g | tr -d '|' | grep -i -v 's:'| grep -i $data_hoje)
 
 for line in $lista
 do
-
+echo $line
+echo $index
 login=$(echo "$line" | cut -d ';' -f 1)
 email=$(echo "$line" | cut -d ';' -f 2)
 date=$(echo "$line" | cut -d ';' -f 3)
@@ -16,12 +22,28 @@ name=$(echo "$line" | cut -d ';' -f 7)
 surname=$(echo "$line" | cut -d ';' -f 8)
 city=$(echo "$line" | cut -d ';' -f 9)
 
+
+
+curl --user $username:$password -XPOST "$elastichost:$elasticport/$index/_mapping/doc" -d "\
+	{
+		"properties": {
+		"date": {
+		"type": "date"
+	}
+}
+}"
+
+curl --user $username:$password -XPOST -H "Content-Type: application/json" "$elastichost:$elasticport/$index/doc?pretty" -d "\
+	{
+		"login": "$login",
+		"email": "$email",
+		"data": "$date",
+		"curso": "$coursename",
+		"nome": "$name",
+		"sobrenome": "$surname",
+		"cidade": "$city"
+	}"
+
+
 done
-
-
-#diff=$(diff $log_tmp $log)
-#if test -n "$diff"
-#	then
-#		echo "$diff" | grep -v -E ^[0-9] | sed s/'< '/':'/g | sed s/'> '/':'/g | tr -d ':'  >> $log
-#	fi
 
