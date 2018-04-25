98d0226e408bc4d9c420f4f488555505e928c03f
brunoodon <bruno.odon@gmail.com>
2018-04-25 11:53:10 -0300

    Update elastic-howto-api.sh

--- elasticsearch/elastic-howto-api.sh
+++ elasticsearch/elastic-howto-api.sh
@@ -6,7 +6,7 @@ elasticport=9200
 username=elastic
 password="$1"
 index=howto-$data_hoje
-mysql -u root -h 91.205.173.217 --password=mujebik@ -D moodle -e "select mdl_user.username,mdl_user.email,mdl_logstore_standard_log.action,from_unixtime(mdl_logstore_standard_log.timecreated, '%Y-%m-%d') as data_acesso, mdl_logstore_standard_log.courseid, mdl_course.fullname, mdl_logstore_standard_log.other, mdl_user.id, mdl_user.firstname, mdl_user.lastname, mdl_user.city from  mdl_logstore_standard_log, mdl_user, mdl_course where mdl_user.id = mdl_logstore_standard_log.userid and mdl_logstore_standard_log.action = 'viewed' and mdl_logstore_standard_log.courseid = mdl_course.id" | grep -v username | tr \\t ';' | cut -d ';' -f 1,2,4,5,6,9- | sed s/';}'/'|'/g | tr -d '|' | grep -i -v 's:'|grep -i "$data_hoje" > /tmp/query.tmp
+mysql -u root -h 91.205.173.217 --password=<password> -D moodle -e "select mdl_user.username,mdl_user.email,mdl_logstore_standard_log.action,from_unixtime(mdl_logstore_standard_log.timecreated, '%Y-%m-%d') as data_acesso, mdl_logstore_standard_log.courseid, mdl_course.fullname, mdl_logstore_standard_log.other, mdl_user.id, mdl_user.firstname, mdl_user.lastname, mdl_user.city from  mdl_logstore_standard_log, mdl_user, mdl_course where mdl_user.id = mdl_logstore_standard_log.userid and mdl_logstore_standard_log.action = 'viewed' and mdl_logstore_standard_log.courseid = mdl_course.id" | grep -v username | tr \\t ';' | cut -d ';' -f 1,2,4,5,6,9- | sed s/';}'/'|'/g | tr -d '|' | grep -i -v 's:'|grep -i "$data_hoje" > /tmp/query.tmp
 
 
 curl --user $username:$password -XPUT -H "Content-Type: application/json" "$elastichost:$elasticport/$index?pretty" \
